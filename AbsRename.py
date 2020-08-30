e6dd2c85726b75e6460a1aa1d0be6c40e90bc40b
gamernayr <gamernayr@gmail.com>
2020-08-30 15:17:39 -0400

    First Commit

---
+++ AbsRename.py
@@ -0,0 +1,77 @@
+import os, shutil
+from glob import glob
+#os.chdir("A:\TV Shows\Mr.Robot\Mr.Robot Season 3\subs")
+location = os.getcwd()
+
+def getSelfFolder(path):
+    path = path.split("\\")#split by folder structure
+    return path[-1]
+
+def run():
+    print("Starting............")
+
+    print("Gathering files.....")
+    #result = [y for x in os.walk(os.getcwd()) for y in glob(os.path.join(x[0], "*"))]
+    for count, filename in enumerate(os.listdir(os.getcwd())):
+        src = os.getcwd() + "\\" + filename#define original filename
+        #print(src)
+        if os.path.isfile:
+            workname = filename.split(" ")#change it into what we want
+
+            #---------------------------------------------------->>>>
+            # OPTION 1 ----- Adding an E for abosolute number episode ordering
+            # This part adds the episode indicator on the number in the name of the file
+            # Python starts list numbering at zero, so "[Judas] ANIME TITLE - 100" could be workname[4], or workname[-1]
+            
+            #workname[5] = "E" + workname[5]
+            
+            #----------------------------------------------------<<<<
+            
+            #---------------------------------------------------->>>>
+            # OPTION 2 ----- Manual labeling of all episodes in a season
+            
+            #maldname = "Mr.Robot S03E" + (str(1+count) if 1+count>=10 else "0" + str(1+count))+ ".srt"
+            
+            #----------------------------------------------------<<<<
+            
+            #for w in workname:
+                #maldname = maldname + w + " "
+            dst = os.getcwd() + "\\" + maldname#define new filename
+            print(maldname)
+            os.rename(src, dst) 
+
+    #print("Adding characters...")
+    #for f in result
+
+print("Working directory: " + location)
+
+while True:
+    parsed = input().split(" ")
+    if parsed[0] == "cd":
+        os.chdir(parsed[1])
+        print(os.getcwd())
+    elif parsed[0] == "cwd":
+        print(os.getcwd())
+    elif parsed[0] == "ls":
+        f = os.listdir(os.getcwd())
+        files = []
+        folders = []
+        for j in f:#for files in list
+            if os.path.isfile(j):
+                files.append(j)
+            else:
+                folders.append(j)
+        #folders first
+        for h in folders:
+            print("[" + h + "]")
+        for h in files:
+            print(h)
+    elif parsed[0] == "run":
+        run()
+        break
+    elif parsed[0] == "exit":
+        break
+
+print("Done")    
+#else:
+#    print("Goodbye")
---
+++ README.md
@@ -0,0 +1,21 @@
+**AbsRename**
+By Ryan Epp
+
+---
+**Features:**
+Batch rename files to comply with media services like Plex or Jellyfin. 
+
+**Example:**
+- Change starting directory in file (or navigate using the built-in file explorer) to folder containing files you want changed
+- Pick an option and change perimeters how you see fit (see code for more details)
+- Run script
+- Navigate around folders, or just type 'run' to start renaming
+
+**Requirements:**
+- Python 3 enabled terminal
+
+**Bugs/Needs Improvement:**
+- The entire process is clunky, but the script is written and ready to be used with few changes
+
+**Notes:**
+Although this was originally meant to be used for changing large amounts of episode filenames, it's a good template for renaming files in numerical order. 
